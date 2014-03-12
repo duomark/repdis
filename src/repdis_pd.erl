ced3779796d8e1cd944a2f1cb2fc1bbcb63e2610
Jay Nelson <jay@duomark.com>
2014-03-12 02:16:41 -0700

    Add hgetall and flesh out rest of redis hashes

--- src/repdis_pd.erl
+++ src/repdis_pd.erl
@@ -14,14 +14,19 @@
 
 %% External interface
 -export([
+         hgetall/1, hgetall/2,
          hget/2, hmget/2, hget/3, hmget/3,
          hset/3, hmset/2, hset/4, hmset/3,
-         hdel/2, hdel/3
+         hdel/2, hdel/3,
+         hexists/2, hexists/3,
+         hkeys/1, hkeys/2, hlen/1, hlen/2,
+         hsetnx/3, hsetnx/4, hvals/1, hvals/2,
+         hincrby/3, hincrby/4, hincrbyfloat/3, hincrbyfloat/4
         ]).
 
 -define(LOW_DB,   0).
 -define(HIGH_DB, 15).
--define(VALID_DB_NUM(__Num), __Num >= ?LOW_DB andalso __Num =< ?HIGH_DB).
+-define(VALID_DB_NUM(__Num), is_integer(__Num) andalso __Num >= ?LOW_DB andalso __Num =< ?HIGH_DB).
 
 %% External functions for testing
 -export([sentinel/0]).
@@ -42,6 +47,21 @@
 %%% External API
 %%%------------------------------------------------------------------------------
 
+-spec hgetall (key() )           -> [binary() | get_value()].  %% Actually pairs flattened.
+-spec hgetall (db_num(), key() ) -> [binary() | get_value()].  %% Actually pairs flattened.
+
+hgetall(Key) ->
+    hgetall(?LOW_DB, Key).
+
+hgetall(Db_Num, Key)
+  when ?VALID_DB_NUM(Db_Num) ->
+    Bin_Key = iolist_to_binary(Key),
+    case get_dict(Db_Num, Bin_Key) of
+        undefined -> [];
+        Dict      -> lists:foldr(fun({Field, Val}, Accum) -> [Field, Val | Accum] end,
+                                 [], dict:to_list(Dict))
+    end.
+
 -spec hget  (key(),  field()  ) ->  get_value().
 -spec hmget (key(), [field()] ) -> [get_value()].
 
@@ -52,14 +72,16 @@ hmget(Key, Fields) -> hmget(?LOW_DB, Key, Fields).
 -spec hget  (db_num(), key(),  field()  ) ->  get_value().
 -spec hmget (db_num(), key(), [field()] ) -> [get_value()].
      
-hget(Db_Num, Key, Field) when ?VALID_DB_NUM(Db_Num) ->
+hget(Db_Num, Key, Field)
+  when ?VALID_DB_NUM(Db_Num) ->
     Bin_Key = iolist_to_binary(Key),
     case get_dict(Db_Num, Bin_Key) of
         undefined -> nil;
         Dict      -> get_field_value(Field, Dict)
     end.
 
-hmget(Db_Num, Key, Fields) when ?VALID_DB_NUM(Db_Num) ->
+hmget(Db_Num, Key, Fields)
+  when ?VALID_DB_NUM(Db_Num) ->
     Bin_Key = iolist_to_binary(Key),
     case get_dict(Db_Num, Bin_Key) of
         undefined -> lists:duplicate(length(Fields), nil);
@@ -75,29 +97,33 @@ hmset(Key, Field_Value_Pairs) -> hmset(?LOW_DB, Key, Field_Value_Pairs).
 
 
 -spec hset  (db_num(), key(), field(), set_value() ) -> 0 | 1.
--spec hmset (db_num(), key(), field_value_pairs()  ) -> ok.
+-spec hmset (db_num(), key(), field_value_pairs()  ) -> binary().
 
-hset(Db_Num, Key, Field, Value) when ?VALID_DB_NUM(Db_Num) ->
+hset(Db_Num, Key, Field, Value)
+  when ?VALID_DB_NUM(Db_Num) ->
     Bin_Key = iolist_to_binary(Key),
     case get_dict(Db_Num, Bin_Key) of
         undefined -> set_value_in_new_dict     (Db_Num, Bin_Key, Field, Value);
         Dict      -> set_value_in_existing_dict(Db_Num, Bin_Key, Field, Value, Dict)
     end.
 
-hmset(Db_Num, Key, Field_Value_Pairs) when ?VALID_DB_NUM(Db_Num) ->
+hmset(Db_Num, Key, Field_Value_Pairs)
+  when ?VALID_DB_NUM(Db_Num) ->
     Bin_Key = iolist_to_binary(Key),
     case get_dict(Db_Num, Bin_Key) of
-        undefined -> set_values_in_new_dict     (Db_Num, Bin_Key, Field_Value_Pairs);
-        Dict      -> set_values_in_existing_dict(Db_Num, Bin_Key, Field_Value_Pairs, Dict)
+        undefined -> set_values_in_new_dict     (Db_Num, Bin_Key, Field_Value_Pairs),       <<"OK">>;
+        Dict      -> set_values_in_existing_dict(Db_Num, Bin_Key, Field_Value_Pairs, Dict), <<"OK">>
     end.
 
 
 -spec hdel(key(), [field()])           -> non_neg_integer().
 -spec hdel(db_num(), key(), [field()]) -> non_neg_integer().
     
-hdel(Key, Fields) -> hdel(?LOW_DB, Key, Fields).
+hdel(Key, Fields) ->
+    hdel(?LOW_DB, Key, Fields).
 
-hdel(Db_Num, Key, Fields) when ?VALID_DB_NUM(Db_Num) ->
+hdel(Db_Num, Key, Fields)
+  when ?VALID_DB_NUM(Db_Num) ->
     Bin_Key = iolist_to_binary(Key),
     case get_dict(Db_Num, Bin_Key) of
         undefined -> 0;
@@ -105,6 +131,124 @@ hdel(Db_Num, Key, Fields) when ?VALID_DB_NUM(Db_Num) ->
                      {Num_Deleted, New_Dict} = lists:foldl(Fold_Fn, {0, Dict}, Fields),
                      Dict = put_dict(Db_Num, Bin_Key, New_Dict),
                      Num_Deleted
+    end.
+
+
+-spec hexists(key(), field())           -> boolean().
+-spec hexists(db_num(), key(), field()) -> boolean().
+
+hexists(Key, Field) ->
+    hexists(?LOW_DB, Key, Field).
+
+hexists(Db_Num, Key, Field)
+  when ?VALID_DB_NUM(Db_Num) ->
+    Bin_Key = iolist_to_binary(Key),
+    case get_dict(Db_Num, Bin_Key) of
+        undefined -> false;
+        Dict      -> get_field_value(Field, Dict) =/= nil
+    end.
+
+
+-spec hkeys (key())           -> [key()].
+-spec hkeys (db_num(), key()) -> [key()].
+-spec hlen  (key())           -> non_neg_integer().
+-spec hlen  (db_num(), key()) -> non_neg_integer().
+
+hkeys(Key) ->
+    hkeys(?LOW_DB, Key).
+
+hkeys(Db_Num, Key)
+  when ?VALID_DB_NUM(Db_Num) ->
+    Bin_Key = iolist_to_binary(Key),
+    case get_dict(Db_Num, Bin_Key) of
+        undefined -> [];
+        Dict      -> dict:fetch_keys(Dict)
+    end.
+
+hlen(Key) ->
+    hlen(?LOW_DB, Key).
+
+hlen(Db_Num, Key)
+  when ?VALID_DB_NUM(Db_Num) ->
+    Bin_Key = iolist_to_binary(Key),
+    case get_dict(Db_Num, Bin_Key) of
+        undefined -> 0;
+        Dict      -> dict:size(Dict)
+    end.
+
+
+-spec hsetnx(key(), field(), set_value())           -> 0 | 1.
+-spec hsetnx(db_num(), key(), field(), set_value()) -> 0 | 1.
+
+hsetnx(Key, Field, Value) ->
+    hsetnx(?LOW_DB, Key, Field, Value).
+
+hsetnx(Db_Num, Key, Field, Value)
+  when ?VALID_DB_NUM(Db_Num) ->
+    Bin_Key = iolist_to_binary(Key),
+    case get_dict(Db_Num, Bin_Key) of
+        undefined -> set_value_in_new_dict(Db_Num, Bin_Key, Field, Value);
+        Dict      -> case get_field_value(Field, Dict) of
+                         undefined -> set_value_in_existing_dict(Db_Num, Bin_Key, Field, Value, Dict);
+                         _Exists   -> 0
+                     end
+    end.
+    
+
+-spec hvals(key())           -> [get_value()].
+-spec hvals(db_num(), key()) -> [get_value()].
+
+hvals(Key) ->
+    hvals(?LOW_DB, Key).
+
+hvals(Db_Num, Key)
+  when ?VALID_DB_NUM(Db_Num) ->
+    Bin_Key = iolist_to_binary(Key),
+    case get_dict(Db_Num, Bin_Key) of
+        undefined -> [];
+        Dict      -> [Value || {_Key, Value} <- dict:to_list(Dict)]
+    end.
+
+
+-spec hincrby(key(), field(), integer()) -> integer().
+-spec hincrby(db_num(), key(), field(), integer()) -> integer().
+-spec hincrbyfloat(key(), field(), float()) -> float().
+-spec hincrbyfloat(db_num(), key(), field(), float()) -> float().
+
+hincrby(Key, Field, Increment)
+  when is_integer(Increment) ->
+    hincrby(?LOW_DB, Key, Field, Increment).
+
+hincrby(Db_Num, Key, Field, Increment)
+  when ?VALID_DB_NUM(Db_Num), is_integer(Increment) ->
+    Bin_Key = iolist_to_binary(Key),
+    case get_dict(Db_Num, Bin_Key) of
+        undefined -> set_value_in_new_dict(Db_Num, Bin_Key, Field, Increment),
+                     Increment;
+        Dict      -> case get_field_value(Field, Dict) of
+                         nil   -> set_value_in_existing_dict(Db_Num, Bin_Key, Field, Increment, Dict);
+                         Value -> New_Value = integer_to_binary(binary_to_integer(Value) + Increment),
+                                  set_value_in_existing_dict(Db_Num, Bin_Key, Field, New_Value, Dict),
+                                  New_Value
+                     end
+    end.
+
+hincrbyfloat(Key, Field, Increment)
+  when is_float(Increment) ->
+    hincrbyfloat(?LOW_DB, Key, Field, Increment).
+
+hincrbyfloat(Db_Num, Key, Field, Increment)
+  when ?VALID_DB_NUM(Db_Num), is_float(Increment) ->
+    Bin_Key = iolist_to_binary(Key),
+    case get_dict(Db_Num, Bin_Key) of
+        undefined -> set_value_in_new_dict(Db_Num, Bin_Key, Field, Increment),
+                     Increment;
+        Dict      -> case get_field_value(Field, Dict) of
+                         nil   -> set_value_in_existing_dict(Db_Num, Bin_Key, Field, Increment, Dict);
+                         Value -> New_Value = float_to_binary(binary_to_float(Value) + Increment),
+                                  set_value_in_existing_dict(Db_Num, Bin_Key, Field, New_Value, Dict),
+                                  New_Value
+                     end
     end.
 
 
@@ -127,36 +271,37 @@ get_field_value(Field, Dict) ->
         {ok, Value} -> Value
     end.
              
-set_value_in_new_dict(Db_Num, Key, Field, Value) ->
-    Bin_Key   = iolist_to_binary(Key),
+set_value_in_new_dict(Db_Num, Bin_Key, Field, Value)
+  when is_binary(Bin_Key) ->
     Bin_Field = iolist_to_binary(Field),
     Bin_Value = iolist_to_binary(Value),
     New_Dict = dict:store(Bin_Field, Bin_Value, dict:new()),
     undefined = put_dict(Db_Num, Bin_Key, New_Dict),
     1.
 
-set_value_in_existing_dict(Db_Num, Key, Field, Value, Dict) ->
-    Bin_Key   = iolist_to_binary(Key),
+set_value_in_existing_dict(Db_Num, Bin_Key, Field, Value, Dict)
+  when is_binary(Bin_Key) ->
     Bin_Field = iolist_to_binary(Field),
     Bin_Value = iolist_to_binary(Value),
     {Result, New_Dict} = set_field_value(Bin_Field, Bin_Value, Dict),
     Dict = put_dict(Db_Num, Bin_Key, New_Dict),
     Result.
 
-set_field_value(Bin_Field, Bin_Value, Dict) ->
+set_field_value(Bin_Field, Bin_Value, Dict)
+  when is_binary(Bin_Field), is_binary(Bin_Value) ->
     case {dict:find(Bin_Field, Dict), dict:store(Bin_Field, Bin_Value, Dict)} of
         {error,   Dict2} -> {1, Dict2};
         {{ok, _}, Dict2} -> {0, Dict2}
     end.
 
-set_values_in_new_dict(Db_Num, Key, Field_Value_Pairs) ->
-    Bin_Key = iolist_to_binary(Key),
+set_values_in_new_dict(Db_Num, Bin_Key, Field_Value_Pairs)
+  when is_binary(Bin_Key) ->
     New_Dict = set_fields(Field_Value_Pairs, dict:new()),
     undefined = put_dict(Db_Num, Bin_Key, New_Dict),
     ok.
 
-set_values_in_existing_dict(Db_Num, Key, Field_Value_Pairs, Dict) ->
-    Bin_Key = iolist_to_binary(Key),
+set_values_in_existing_dict(Db_Num, Bin_Key, Field_Value_Pairs, Dict)
+  when is_binary(Bin_Key) ->
     New_Dict = set_fields(Field_Value_Pairs, Dict),
     Dict = put_dict(Db_Num, Bin_Key, New_Dict),
     ok.
